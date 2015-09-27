d91739bad4efc45bef5d4568db6d370b4e792e74
曾小堤 <cengxiaodi@cengxiaodideMacBook-Air.local>
2015-09-27 15:09:40 +0800

    add button and textfield

--- MVCSample/ViewController.h
+++ MVCSample/ViewController.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
+@property (weak,nonatomic) IBOutlet UIButton *myButton;
+@property (weak,nonatomic) IBOutlet UITextField *myTextField;
 
+-(IBAction)myAction:(id)sender;
 
 @end
 
--- MVCSample/ViewController.m
+++ MVCSample/ViewController.m
@@ -15,6 +15,8 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
+    [super viewDidLoad];
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -23,5 +25,50 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewDidUnload {
+    [self setMyButton:nil];
+    [self setMyTextField:nil];
+    [super viewDidUnload];
+}
+
+- (IBAction)myAction:(id)sender {
+    
+    self.myTextField.text = @"一开始就在输入框的文字";
+    
+    NSLog(@"call myAction:");
+    
+}
+
+#pragma mark -- UITextFieldDelegate method
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    NSLog(@"call textFieldShouldBeginEditing:");
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    NSLog(@"call textFieldDidBeginEditing:");
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField
+{
+    NSLog(@"call textFieldShouldEndEditing:");
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    NSLog(@"call textFieldDidEndEditing:");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"call textFieldShouldReturn:");
+    [textField resignFirstResponder];
+    return YES;
+}
+
 
 @end
